f4972072ddbd90453e6fb04aba661768ba70b096
Ariya Saputra <55862687+ariyazz@users.noreply.github.com>
2021-06-26 23:24:34 +0700

    Add files via upload

---
+++ crack.py
@@ -0,0 +1,9 @@
+import os, time, sys
+from time import sleep as waktu
+def menit(s):
+	for c in s + "\n":
+		sys.stdout.write(c)
+		sys.stdout.flush()
+		time.sleep(0.10)
+os.system("clear")
+menit("SCRIPT SEDANG DALAM PERBAIKAN\nFOLLOW GITHUB SAYA UNTUK\nMENGETAHUI INFO UPDATE SCRIPT SELANJUTNYA\n\033[1;92mGithub : https://github.com/ariyazz")
